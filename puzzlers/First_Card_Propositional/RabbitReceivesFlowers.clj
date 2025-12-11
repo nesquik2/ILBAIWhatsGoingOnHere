26c5e3ea7e35198723c4d302e33f2dc669e15531
Vanessa H <hwvanessa14@gmail.com>
2025-12-11 12:45:35 -0500

    Comment out non-propositional assumption A5
    Commented out A5 assumption for later review.

--- puzzlers/First_Card_Propositional/RabbitReceivesFlowers.clj
+++ puzzlers/First_Card_Propositional/RabbitReceivesFlowers.clj
@@ -3,6 +3,5 @@
                A2 (if B_gives_flowers_to_A A_receives_flowers)
                A3 (if B_holds_flowers_for_A B_gives_flowers_to_A)
                A4 B_holds_flowers_for_A
-              ;not propositional!!!can revisit this later though A5 (if (gives_flowers x y) (friends x y))
                 }
  :goal (and A_happy (implies B_holds_flowers_for_A A_happy))}   
