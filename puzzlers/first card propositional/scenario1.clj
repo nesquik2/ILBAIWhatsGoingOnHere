48fd347290f31cdbc1da5fc80ab2d3c9ab2268f7
jaylizhao <zhaoj17@rpi.edu>
2025-12-11 12:37:22 -0500

    Deleted folder

--- puzzlers/first card propositional/scenario1.clj
+++
@@ -1,9 +0,0 @@
-{:assumptions
- {1 p                       ;; p: dog1 is in a tree on a loose branch
-  2 (if p d)                ;; if p, then dog1 is in danger
-  3 (if d s)                ;; if dog1 is in danger, dog2 shouts
-  4 c                       ;; c: fox can help dog1
-  5 (if (and d c) h)}       ;; if dog1 in danger and fox can help, fox should help
-
- :goal
- (and d s h)}               ;; dog1 is in danger, dog2 shouts, fox should help
